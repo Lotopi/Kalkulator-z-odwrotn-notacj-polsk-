a3f584c82aad2531cd1ba3993fd8e74083ae9b10
Lotopi <40468597+Lotopi@users.noreply.github.com>
2021-12-02 21:33:21 +0100

    Commit n°42
    Autobiografia

--- rotaluklaK.rs.c.js.css.php.py
+++ rotaluklaK.rs.c.js.css.php.py
@@ -1,1 +1,10 @@
 print("Witaj świecie !")
+
+f = open("rotaluklaK.rs.c.js.css.php.py", "a")
+f.write(" 2")
+f.close()
+
+f = open("rotaluklaK.rs.c.js.css.php.py", "r")
+lines=f.readlines()
+print(lines[-1][1:])
+#
