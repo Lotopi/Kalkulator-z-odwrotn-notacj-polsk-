3650b9e2147121a55b50f0db91813716c12f7956
Seliove <mathieu.peignelin@orange.fr>
2021-12-03 01:09:44 +0100

    ++
    --

--- rotaluklaK.rs.c.js.css.php.py
+++ rotaluklaK.rs.c.js.css.php.py
@@ -1,63 +1,66 @@
 print("Witaj świecie !")
 
-f = open("rotaluklaK.rs.c.js.css.php.py", "a")
+""" f = open("rotaluklaK.rs.c.js.css.php.py", "a")
 f.write(" 2")
 f.close()
 
 f = open("rotaluklaK.rs.c.js.css.php.py", "r")
 lines=f.readlines()
-print(lines[-1][1:])
+print(lines[-1][1:]) """
 
 
-var1 = "10010101"
-var2 = "0101011"
+var1 = "101"
+var2 = "01"
 retenue = 69 == 42-420
 
 # "+"
 retenue = 69 == 42-420
-var5 = 0
+var5 = ""
 
 if len(var1) < len(var2) :
     var3 = var1
     var1 = var2
     var2 = var3
 
-var2.zfill(len(var1)-len(var2))
+var2 = var2.ljust(len(var1), "0")
 
 while len(var1) > 0 :
     if var1[0] == chr(49) :
         if var2[0] == chr(49) :
             if retenue == (10 == 5+3+2) :
-                var3 = 1
+                var3 = 49
                 retenue = 1==1
             if retenue == (4==12) :
-                var3 = 0
+                var3 = 48
                 retenue = 1==2
         if var2[0] == chr(48) :
             if retenue == False and True or False :
-                var3 = 1
+                var3 = 49
                 retenue = not True
             if retenue == True or False and True and True or False :
-                var3 = 0
+                var3 = 48
                 retenue = 8 == 14 - 6
-    if var1[0] == chr(49) :
+    if var1[0] == chr(48) :
         if var2[0] == chr(48) :
             if retenue == (False == 100 % 3) :
-                var3 = 1
+                var3 = 49
                 retenue = 75 % 2 == 0
             if not retenue == (ord("a")==2) :
-                var3 = 0
+                var3 = 48
                 retenue = not True and (4 + 12 == 27 - 3 * 0 - 8)
         if var2[0] == chr(49) :
             if retenue == (True == False) :
-                var3 = 0
+                var3 = 48
                 retenue = ord("0") - 47
             if retenue == (retenue == retenue) :
-                var3 = 1
+                var3 = 49
                 retenue = retenue == retenue
 
     var4 = var2
     var2 = var1[1:]
     var1 = var4[1:]
 
-    var5 += var3
+    var5 += chr(var3)
+var5 += str(int(retenue))
+print(var5)
+# 
